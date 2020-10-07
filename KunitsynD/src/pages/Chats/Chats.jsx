41b16ab7a093804b9c5539e61cdc29070a4d07f8
Motive-sx <sx.motive@gmail.com>
2020-10-07 13:35:23 +0500

    lesson 7

--- KunitsynD/src/pages/Chats/Chats.jsx
+++ KunitsynD/src/pages/Chats/Chats.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-shadow */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
@@ -6,72 +7,39 @@ import { connect } from 'react-redux';
 import MessageList from '../../components/MessageList';
 import FormMessage from '../../components/FormMessage';
 import Layout from '../../components/Layout/Layout';
-import { addMessage } from '../../reducers/messagesReducer';
+import { asyncAddMessage } from '../../reducers/messagesReducer';
+import {
+  getActiveMessages,
+  getCurrentMessages,
+  getIsFetching,
+} from '../../selectors/chatsSelectors';
+import Preloader from '../../components/Preloader/Preloader';
+import { fetchChats } from '../../reducers/chatReducer';
 
 class Chats extends Component {
-  // componentDidUpdate() {
-  //   const lastMessages = this.messages;
-
-  //   if (lastMessages[lastMessages.length - 1]?.author !== 'Bot') {
-  //     setTimeout(() => {
-  //       this.addMessage({ author: 'Bot', message: 'привет, я бот' });
-  //     }, 500);
-  //   }
-  // }
-
-  addMessage = ({ author, message }) => {
-    const {
-      match: {
-        params: { id },
-      },
-    } = this.props;
-    const newId = uuidv4();
-
-    this.setState(({ chats, messages }) => ({
-      chats: {
-        ...chats,
-        [id]: { ...chats[id], messageList: [...chats[id].messageList, newId] },
-      },
-      messages: { ...messages, [newId]: { id: newId, author, message } },
-    }));
-
-    // with Immer.js
-    // this.setState(
-    //   produce(draft => {
-    //     draft.chats[id].messageList.push(newId);
-    //     draft.messages[newId] = { id: newId, author, message };
-    //   }),
-    // );
-  };
-
-  addChat = () => {
-    const newId = uuidv4();
-    this.setState(({ chats }) => ({
-      chats: { ...chats, [newId]: { id: newId, title: `Чат ${newId}`, messageList: [] } },
-    }));
-  };
-
-  get messages() {
-    const {
-      match: {
-        params: { id },
-      },
-      chats,
-      messages,
-    } = this.props;
-
-    if (id in chats) {
-      return chats[id].messageList.map(messId => messages[messId]);
-    }
-    return [];
+  componentDidMount() {
+    const { fetchChats: asyncFetchChats } = this.props;
+    asyncFetchChats();
   }
 
+  submitMessage = ({ author, message }) => {
+    const {
+      asyncAddMessage,
+      match: {
+        params: { id },
+      },
+    } = this.props;
+    asyncAddMessage({ author, message, chatId: id, id: uuidv4() });
+  };
+
   render() {
+    const { messages, activeMessages, isFetching } = this.props;
+
     return (
       <Layout>
-        <MessageList messages={this.messages} />
-        <FormMessage addMessage={this.addMessage} />
-        <button onClick={() => this.props.addMessage()}>add message</button>
+        <Preloader open={isFetching} />
+        <MessageList messages={messages} activeMessages={activeMessages} />
+        <FormMessage addMessage={this.submitMessage} />
       </Layout>
     );
   }
@@ -81,15 +49,30 @@ Chats.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.objectOf(PropTypes.any),
   }).isRequired,
+  messages: PropTypes.arrayOf(PropTypes.any).isRequired,
+  activeMessages: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.string, PropTypes.number]))
+    .isRequired,
+  asyncAddMessage: PropTypes.func.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+  fetchChats: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = state => ({
-  chats: state.chats.byIds,
-  messages: state.messages.byIds,
-});
+const mapStateToProps = (state, ownProps) => {
+  const {
+    match: {
+      params: { id },
+    },
+  } = ownProps;
+  return {
+    messages: getCurrentMessages(state, id),
+    activeMessages: getActiveMessages(state),
+    isFetching: getIsFetching(state),
+  };
+};
 
 const mapDispatchToProps = {
-  addMessage,
+  asyncAddMessage,
+  fetchChats,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Chats);
